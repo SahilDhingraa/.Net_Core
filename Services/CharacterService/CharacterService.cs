3ef1459cca8cbdc204b73b98d0127bc2cf0e25b4
Sahil Dhingra <dhingrasd451@gmial.com>
2022-09-23 14:23:05 +0530

    course 3.32

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using udemy1.Services.CharacterService;
 using udemy1.Dtos.Character;
+using udemy1.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace udemy1.Services.CharacterService
 {
@@ -11,10 +13,12 @@ namespace udemy1.Services.CharacterService
             new Character{Id = 1, Name = "Alaudin Khilji", HitPoints = 10000}
         };
         private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
@@ -47,14 +51,18 @@ namespace udemy1.Services.CharacterService
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
-            return new ServiceResponse<List<GetCharacterDto>> { Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList() };
+            var response = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            response.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+
+            return response;
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var ServiceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            ServiceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            ServiceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
             return ServiceResponse;
 
         }
