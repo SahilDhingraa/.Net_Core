174104bf4efa20f457937de76a642b7172e63058
Sahil Dhingra <dhingrasd451@gmial.com>
2022-09-24 17:56:03 +0530

    course 4.41

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,10 +4,7 @@ namespace udemy1.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-            
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
     }
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using udemy1.Services.CharacterService;
+
+namespace udemy1.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user,String password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
