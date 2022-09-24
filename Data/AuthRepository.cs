772a697df5fdb6d4b9ee0983efce4cf82ef46c90
Sahil Dhingra <dhingrasd451@gmial.com>
2022-09-24 18:17:17 +0530

    cousre 4.43

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -2,25 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using udemy1.Services.CharacterService;
 
 namespace udemy1.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            ServiceResponse<int> response = new ServiceResponse<int>();
+
+            if(await UserExists(user.Usernmae))
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+            CreatePAsswordHash(password,out byte[] passwordHash,out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            return (await _context.Users.AnyAsync(udemy1 => udemy1.Usernmae.ToLower() == username.ToLower()));
+        }
+
+        private void CreatePAsswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using (var hmac= new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
