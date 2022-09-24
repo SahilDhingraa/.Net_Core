e8788f30f03e83b5a8c0db4b6d606e891e9f7c26
Sahil Dhingra <dhingrasd451@gmial.com>
2022-09-24 18:45:02 +0530

    course 4.44

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using udemy1.Data;
+using udemy1.Dtos.User;
+using udemy1.Services.CharacterService;
+
+namespace udemy1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var responnse = await _authRepo.Register(
+                new User { Usernmae = request.Username}, request.password
+            );
+            if(!responnse.Success)
+            {
+                return BadRequest(responnse);
+            }
+            return Ok(responnse);
+        }
+    }
+}
--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using udemy1.Services.CharacterService;
 
 namespace udemy1.Data
